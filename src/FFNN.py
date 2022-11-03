4a4df44027d906f79131832659a090e6af99f566
erlend1202 <erlendkri1202@gmail.com>
2022-11-03 12:28:11 +0100

    finished multi layer neural network, works fine

--- src/FFNN.py
+++ src/FFNN.py
@@ -43,10 +43,9 @@ def learning_schedule(t):
 
 
 class FeedForwardNeuralNetwork:
-    def __init__(self, X, Y, layers, n_hidden_neurons = 50, n_categories = 1, batch_size = 100, eta = 0.1, lmbda = 0.0, epochs = 10):
+    def __init__(self, X, Y, layers, n_categories = 1, batch_size = 100, eta = 0.1, lmbda = 0.0, epochs = 10):
         self.X = X
         self.Y = Y
-        self.n_hidden_neurons = n_hidden_neurons
         self.n_inputs = X.shape[0] # Samples
         self.n_features = X.shape[1]
         self.n_categories = n_categories
@@ -78,12 +77,6 @@ class FeedForwardNeuralNetwork:
         self.weights.append(w)
         self.bias.append(b)
              
-
-        self.hidden_weights = np.random.randn(self.n_features, self.n_hidden_neurons)
-        self.hidden_bias = np.zeros(self.n_hidden_neurons) + 0.01
-
-        self.output_weights = np.random.randn(self.n_hidden_neurons, self.n_categories)
-        self.output_bias = np.zeros(self.n_categories) + 0.01
     
     def feed_forward(self): 
         self.z = []
@@ -104,25 +97,30 @@ class FeedForwardNeuralNetwork:
         self.z.append(z)
         self.a.append(z)
     
-        #self.probabilities = z
+        self.probabilities = z
 
-        self.z_h = np.matmul(self.current_X_data, self.hidden_weights) + self.hidden_bias
-        self.a_h = sigmoid(self.z_h)
-        
-        self.z_o = np.matmul(self.a_h, self.output_weights) + self.output_bias
-        
-        self.probabilities = self.z_o
 
     def feed_forward_out(self, X):
-        z_h = np.matmul(X, self.hidden_weights) + self.hidden_bias
-        a_h = sigmoid(z_h)
-        z_o = np.matmul(a_h, self.output_weights) + self.output_bias
+        z_list = []
+        a_list = []
 
-        return z_o
+        for i in range(self.num_layers):
+            if i == 0:
+                z = np.matmul(X, self.weights[i]) + self.bias[i]
+                a = sigmoid(z)
+            else:
+                z = np.matmul(a_list[i-1], self.weights[i]) + self.bias[i]
+                a = sigmoid(z)
+
+            z_list.append(z)
+            a_list.append(a)
+            
+        z = np.matmul(a_list[-1], self.weights[-1]) + self.bias[-1]
+    
+        return z
     
     def backpropagation(self):
-        prob = self.z[-1] 
-        error1 = prob - self.current_Y_data
+        error1 = self.probabilities - self.current_Y_data
         errors = [error1]
         self.w_grads = []
         self.bias_grads = []
@@ -149,24 +147,6 @@ class FeedForwardNeuralNetwork:
             self.weights[i] -= self.eta * self.w_grads[self.num_layers-i]
             self.bias[i] -= self.eta * self.bias_grads[self.num_layers-i]
 
-        error_output = self.probabilities - self.current_Y_data
-        error_hidden = np.matmul(error_output, self.output_weights.T) * self.a_h * (1 - self.a_h)
-        #print(error_output.shape, self.output_weights.T.shape, self.a_h.shape)
-        #print(error_hidden.shape)
-        self.output_weights_gradient = np.matmul(self.a_h.T, error_output)
-        self.output_bias_gradient = np.sum(error_output, axis=0)
-
-        self.hidden_weights_gradient = np.matmul(self.current_X_data.T, error_hidden)
-        self.hidden_bias_gradient = np.sum(error_hidden, axis=0)
-
-        if self.lmbda > 0.0:
-            self.output_weights_gradient += self.lmbda * self.output_weights
-            self.hidden_weights_gradient += self.lmbda * self.hidden_weights
-        
-        self.output_weights -= self.eta * self.output_weights_gradient
-        self.output_bias -= self.eta * self.output_bias_gradient
-        self.hidden_weights -= self.eta * self.hidden_weights_gradient
-        self.hidden_bias -= self.eta * self.hidden_bias_gradient
 
 
     def predict(self, X):
@@ -213,6 +193,7 @@ if __name__ == "__main__":
 """
 
 if __name__ == "__main__":
+    t0, t1 = 5, 50 #not used yet
     #n = 1000
     n = 100
     dim = 2
@@ -224,8 +205,8 @@ if __name__ == "__main__":
     y_exact = 4 + 3*x + x ** 2 
     y = y_exact + np.random.randn(n,1)*0.1
 
-    layers = [5, 8, 13, 8]
-    nn = FeedForwardNeuralNetwork(X, y, layers, 3, 1, 10, epochs=10)
+    layers = [5,3]
+    nn = FeedForwardNeuralNetwork(X, y, layers, 1, 10, epochs=1000)
     nn.train()
     #plt.plot(x, y, label="noise")
     plt.plot(x, y_exact, label="exact")
